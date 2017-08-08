87cb36fd956aa680c0bb7e7e8b4bd63376345413
xskh2007 <261767353@qq.com>
2017-08-08 11:03:37 +0800

    deploy all

---
+++ deploy/tasks.py
@@ -0,0 +1,56 @@
+# -*- coding:utf-8 -*-
+from __future__ import absolute_import
+from celery import task
+
+
+
+# pro_name="dsd"
+
+@task
+def mycl_deploy(env):
+    from jenkinsapi.jenkins import Jenkins as Jenkins2
+
+    import jenkins
+    giturl = "git@gitlab.lark.wiki:nbgold/"
+    template_xml = "/root/jumpserver-master/tmp/jenkins_job.xml"
+    view_name = env
+    server = jenkins.Jenkins('http://192.168.2.169:8080/jenkins/', username='admin', password='zzjr#2015')
+
+    server2 = Jenkins2('http://192.168.2.169:8080/jenkins/', username='admin', password='zzjr#2015')
+
+    import xml.etree.cElementTree as ET
+    job_list = ["bank", "mldp", "pms", "cms", "pay", "member", "weapon", "trade", "wealth", "statistics", "bops",
+                "napp", "site", "crm", "channel", "tiger", "rams", "activity", "api", "eagle", "squirrel", "wap",
+                "dragon", "tengu"]
+    tree = ET.ElementTree(file=template_xml)
+
+    def create_job_file(model):
+        # for ele in tree.iterfind("properties/hudson.model.ParametersDefinitionProperty/parameterDefinitions/hudson.model.StringParameterDefinition/defaultValue"):
+        for ele in tree.findall("properties/hudson.model.ParametersDefinitionProperty/parameterDefinitions/hudson.model.StringParameterDefinition/defaultValue"):
+            ele.text = model
+            ele.set("updated", "up")
+        # for ele in tree.iterfind("scm/userRemoteConfigs/hudson.plugins.git.UserRemoteConfig/url"):
+        for ele in tree.findall("scm/userRemoteConfigs/hudson.plugins.git.UserRemoteConfig/url"):
+            ele.text = giturl + model + ".git"
+            ele.set("updated", "up")
+        file_name = "/root/jumpserver-master/tmp/jenkins_job_" + model + ".xml"
+        tree.write(file_name)
+        return open(file_name).read()
+
+    for i in job_list:
+        EMPTY_JOB_CONFIG = create_job_file(i)
+        # print EMPTY_JOB_CONFIG
+
+        ### 批量创建job
+        # new_job = server.create_job(view_name+i, EMPTY_JOB_CONFIG)
+
+        ### 批量发布job by python-jenkins
+        # parameters = {"model":i,"branch":"origin/master"}
+        # deploy=server.build_job(view_name+i,parameters=parameters)
+
+        ### 批量发布job by jenkinsapi
+        parameters = {"model": i, "branch": "origin/master"}
+        deploy = server2.build_job(view_name + i, params=parameters)
+
+        ### 批量删除job
+        # del_job=server.delete_job(view_name+i)
--- deploy/views.py
+++ deploy/views.py
@@ -1,13 +1,17 @@
+# -*- coding:utf-8 -*-
+
 from django.shortcuts import render
 from juser.user_api import *
 # Create your views here.
-
+from deploy.tasks import mycl_deploy
 
 def deploy_all(request):
     # return HttpResponse("okkkk")
     if request.method == 'POST':
-        print request.POST.getlist('qiantu')
-        print "qqqqqq"
+        myenv= request.POST.get('env')
+        mycl_deploy.delay(myenv)
+        message=myenv+"发布中..."
+        return HttpResponse(myenv)
     return my_render('deploy/deploy_all.html', locals(), request)
 
 def deploy_increment(request):
